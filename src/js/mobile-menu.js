ff534229389806530e2458db5f36b1213c6c430c
Kateryna <katyaukr2014@gmail.com>
2025-05-14 19:48:04 +0300

    fix

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,48 +1,50 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const menuBtn = document.querySelector('.menu-button');
-    const menuBtnIcon = menuBtn.querySelector('use');
-    const modalMenu = document.querySelector('.modal');
-    const menuItem = document.querySelectorAll('.menu-list-item');
-    const header = document.querySelector('.header');
-  
-    let isMenuOpen = false;
-  
-    menuBtn.addEventListener('click', () => {
-      isMenuOpen = !isMenuOpen;
-  
-      if (isMenuOpen) {
-        openModal();
-        menuBtnIcon.setAttribute('href', './img/sprite.svg#icon-cross');
-      } else {
-        closeModal();
-        menuBtnIcon.setAttribute('href', './img/sprite.svg#icon-burger');
-      }
-    });
-  
-    menuItem.forEach(item =>
-      item.addEventListener('click', () => {
-        closeModal();
-        menuBtnIcon.setAttribute('href', './img/sprite.svg#icon-burger');
-        isMenuOpen = false;
-      })
-    );
-  
-    window.addEventListener('scroll', blurHeader);
-  
-    function openModal() {
-      modalMenu.style.display = 'block';
-    }
-  
-    function closeModal() {
-      modalMenu.style.display = 'none';
-    }
-  
-    function blurHeader() {
-      if (!header) return;
-      if (window.scrollY > 60) {
-        header.classList.add('header-scroll');
-      } else {
-        header.classList.remove('header-scroll');
-      }
+  const menuBtn = document.querySelector('.menu-button');
+  const menuBtnIcon = menuBtn.querySelector('use');
+  const modalMenu = document.querySelector('.modal');
+  const menuItem = document.querySelectorAll('.menu-list-item');
+  const header = document.querySelector('.header');
+
+  const spriteBasePath = `${window.location.origin}/img/sprite.svg`;
+
+  let isMenuOpen = false;
+
+  menuBtn.addEventListener('click', () => {
+    isMenuOpen = !isMenuOpen;
+
+    if (isMenuOpen) {
+      openModal();
+      menuBtnIcon.setAttribute('href', `${spriteBasePath}#icon-cross`);
+    } else {
+      closeModal();
+      menuBtnIcon.setAttribute('href', `${spriteBasePath}#icon-burger`);
     }
   });
+
+  menuItem.forEach(item =>
+    item.addEventListener('click', () => {
+      closeModal();
+      menuBtnIcon.setAttribute('href', `${spriteBasePath}#icon-burger`);
+      isMenuOpen = false;
+    })
+  );
+
+  window.addEventListener('scroll', blurHeader);
+
+  function openModal() {
+    modalMenu.style.display = 'block';
+  }
+
+  function closeModal() {
+    modalMenu.style.display = 'none';
+  }
+
+  function blurHeader() {
+    if (!header) return;
+    if (window.scrollY > 60) {
+      header.classList.add('header-scroll');
+    } else {
+      header.classList.remove('header-scroll');
+    }
+  }
+});
